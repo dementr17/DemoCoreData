9dcb8856c3ad9f9d88030921a668df2ef6bff300
Dmitry <dementr17@yandex.ru>
2022-01-26 18:52:42 +0300

    удалил комментарии

--- CoreDataDemo/StorageManager.swift
+++ CoreDataDemo/StorageManager.swift
@@ -18,7 +18,6 @@ class StorageManager {
     func saveTask(context: NSManagedObjectContext) {
         do {
             try context.save()
-            //если все нормально то вызываем свойство save
         } catch let error {
             print(error)
         }
@@ -35,13 +34,9 @@ class StorageManager {
     }
     
     func addTask(_ taskName: String, context: NSManagedObjectContext){
-        //Task это сущность в файле CoreDataDemo, в котором описана модель данных. В нашем случае это какбы структура с оинм параметром name типа string
         let task = Task(context: context)
-        //инициализируем экземляр через параметр контекст
         task.name = taskName
-        //переданный в метод save task присваиваем параметру экземпляра
         taskList.append(task)
-        //добавляем в массив элемент
         saveTask(context: context)
     }
     
@@ -51,29 +46,21 @@ class StorageManager {
         
         if let result = try? context.fetch(fetchRequest) {
             for object in result {
-                //Please check before delete operation
                 let task = taskList[indexPath.row]
                 if object == task {
                     context.delete(object)
                 }
             }
         }
-            //перед удалением получаем данные
             taskList.remove(at: indexPath.row)
-            //удаляем данные из массива
-//            print("Posle ", taskList.count)
             saveTask(context: context)
     }
     
     func editTask(_ taskName: String, context: NSManagedObjectContext, indexPath: IndexPath) {
         let task = Task(context: context)
-        //инициализируем экземляр через параметр контекст
         task.name = taskName
-        //переданный в метод save task присваиваем параметру экземпляра
 
         taskList[indexPath.row] = task
-        
-        //добавляем в массив элемент
         saveTask(context: context)
     }
     
@@ -81,6 +68,7 @@ class StorageManager {
 
     var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "CoreDataDemo")
+        
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
 
--- CoreDataDemo/TaskListViewController.swift
+++ CoreDataDemo/TaskListViewController.swift
@@ -12,40 +12,31 @@ class TaskListViewController: UITableViewController {
     private let context = StorageManager.shared.persistentContainer.viewContext
     
     private let cellID = "task"
-    //id ячейки
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .white
-        //фон вью
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
-        //регистрируем ячейку по индексу
         setupNavigationBar()
-        //настраиваем навигейшн бар
         StorageManager.shared.fetchTask(context: context)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
         StorageManager.shared.fetchTask(context: context)
-        //загружаем данные из CoreData и обновляем экран
-        //вызывывается при выгрузке экрана из памяти
         tableView.reloadData()
     }
  
     private func setupNavigationBar() {
         title = "Task List"
-        //текст заголовка
         navigationController?.navigationBar.prefersLargeTitles = true
-        //вроде бы большой заголовок
         
         let navBarAppearence = UINavigationBarAppearance()
-        //экземпляр настроек
         navBarAppearence.configureWithOpaqueBackground()
         navBarAppearence.titleTextAttributes = [.foregroundColor: UIColor.white]
-        //задаем цвет текста для заголовка, когда при прокрутке бар сжимается
         navBarAppearence.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-        //задаем цвет текста для большого заголовка
         
         navBarAppearence.backgroundColor = UIColor(
             red: 21/255,
@@ -53,48 +44,40 @@ class TaskListViewController: UITableViewController {
             blue: 192/255,
             alpha: 194/255
         )
-        //цвет для нав бара
         
         navigationController?.navigationBar.standardAppearance = navBarAppearence
         navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearence
-        //применяем настройки навбара
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(
             barButtonSystemItem: .add,
             target: self,
             action: #selector(addNewTask)
         )
-        //настройки кнопки навбара (тип, где срабатывает, по какому методу)
         
         navigationController?.navigationBar.tintColor = .white
-        //ее цвет
     }
     
     @objc private func addNewTask() {
         showAlert(with: "New Task", and: "What do you want to do?")
-        //вызов алерта при нажатии на кнопку +, передача в алерт заголовка и описания
     }
-    
-   
 }
 
 extension TaskListViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         StorageManager.shared.taskList.count
-        //количество ячеек равно количеству элементов массива
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
-        //инициализируем ячейку через идентификатор
         let task = StorageManager.shared.taskList[indexPath.row]
-        //берем элемент из массива
+        
         var content = cell.defaultContentConfiguration()
         content.text = task.name
         cell.contentConfiguration = content
+        
         return cell
     }
-    //свайп по ячейке
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
@@ -131,19 +114,14 @@ extension TaskListViewController {
 // MARK: AlertController
 extension TaskListViewController {
     
-    private func showAlert(with title: String, and message: String, tf: String = "", index: Int = 0, parametr: Bool = true) {
-        //алерт принимает заголовок и сообщение
-        let index = index
-        let parametr = parametr
+    private func showAlert(with title: String, and message: String, tf: String = "", index: Int = 0) {
         
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
         let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
             guard let task = alert.textFields?.first?.text, !task.isEmpty else { return }
-            //передаем значение из текстового поля в task если оно не пустое по нажатию на сейв
-            self.save(task, index: index, parametr: parametr)
-            //в метод передаем task
+            self.save(task, index: index)
         }
-        
         let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
         
         alert.addAction(saveAction)
@@ -152,38 +130,27 @@ extension TaskListViewController {
             textField.text = tf
             textField.placeholder = "New Task"
         }
-        //настраиваем алерт: кнопки и текстовое поле с подсказкой
         present(alert, animated: true)
-        //вызов алерта
     }
-    private func save(_ taskName: String, index: Int, parametr: Bool) {
-        let index = index
-        let parametr = parametr
+    
+    private func save(_ taskName: String, index: Int) {
         
         var cellIndex: IndexPath!
         StorageManager.shared.addTask(taskName, context: context)
         let taskList = StorageManager.shared.taskList
         
-        if parametr == true {
-            cellIndex = IndexPath(row: taskList.count - 1, section: 0)
-        } else {
-            cellIndex = IndexPath(row: index, section: 0)
-        }
+        cellIndex = IndexPath(row: taskList.count - 1, section: 0)
         
         tableView.insertRows(at: [cellIndex], with: .automatic)
-        //теперь добавляем ячейку в конец таблицы по массиву индексов
     }
     
     private func showAlert1(with title: String, and message: String, tf: String = "", index: Int = 0) {
-        //алерт принимает заголовок и сообщение
         let index = index
         
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
             guard let task = alert.textFields?.first?.text, !task.isEmpty else { return }
-            //передаем значение из текстового поля в task если оно не пустое по нажатию на сейв
             self.save1(task, index: index)
-            //в метод передаем task
         }
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
@@ -194,16 +161,14 @@ extension TaskListViewController {
             textField.text = tf
             textField.placeholder = "New Task"
         }
-        //настраиваем алерт: кнопки и текстовое поле с подсказкой
         present(alert, animated: true)
-        //вызов алерта
     }
+    
     private func save1(_ taskName: String, index: Int) {
         let index = index
         
         var cellIndex = IndexPath(row: index + 1, section: 0)
         StorageManager.shared.editTask(taskName, context: context, indexPath: cellIndex)
-//        let taskList = StorageManager.shared.taskList
         tableView.insertRows(at: [cellIndex], with: .automatic)
         cellIndex.row +=  1
         tableView.deleteRows(at: [cellIndex], with: .automatic)
